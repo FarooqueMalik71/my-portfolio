94debcd370625046be5bb5b7cce142b2d7038b40
FarooqueMalik71 <farooquemalikbusniess@gmail.com>
2025-01-09 06:38:19 +0500

    fix 0.2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,23 +6,35 @@ import Link from 'next/link'
 
 export const Hero = () => {
     return (
-        <div id='hero' className='min-h-screen bg-[url(/profile-pic.png)] bg-no-repeat bg-cover'
-            style={{ backgroundSize: "40%", backgroundPosition: "left 100px top 100px" }}>
-            <Navbar />
+        <>
+            <div id='hero' className='min-h-screen bg-slate-900'>
+                <Navbar />
 
-            <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-                <div className='hidden lg:block'></div>
-                <div className='text-[65px] sm:text-[85px] font-bold leading-tight flex justify-center items-center'>
-                    <div>
-                        <p data-aos="zoom-in-up">I'm</p>
-                        <p data-aos="zoom-in-up">Farooque</p>
-                        <p data-aos="zoom-in-up">Malik</p>
+                <div className='container mx-auto grid sm:grid-cols-1 lg:grid-cols-2 h-[calc(111vh-7.1875rem)] items-center px-4'>
+                    {/* Profile Picture Section */}
+                    <div
+                        className=' relative w-full h-full flex justify-center items-center'
+                    >
+                        {/* Profile Picture */}
+                        <img
+                            src="/profile-pic.png"
+                            alt="Profile"
+                            className=" lg:block md:w-[80%] bg-no-repeat bg-cover rounded-full shadow-lg"
+                        />
+                    </div>
+
+                    {/* Text Section */}
+                    <div className='text-center lg:text-left text-[2.5rem] sm:text-[3.125rem] md:text-[4.0625rem] font-bold leading-tight'>
+                        <div>
+                            <p data-aos="zoom-in-up">I'm</p>
+                            <p data-aos="zoom-in-up">Farooque</p>
+                            <p data-aos="zoom-in-up">Malik</p>
+                        </div>
                     </div>
                 </div>
             </div>
 
-
-            <div className='flex justify-center items-center space-x-4 m-20 ' data-aos="zoom-in-up">
+            <div className='flex justify-center items-center pt-8 space-x-4 m-20 ' data-aos="zoom-in-up">
                 <Link href="https://my-resume-phi-snowy.vercel.app/">
                     <button className="px-2 py-2 bg-black text-white text-lg font-bold rounded-lg border-2 border-accent-500 relative overflow-hidden group">
                         <span className="absolute inset-0 bg-gradient-to-r from-transparent to-teal-500 opacity-0 group-hover:opacity-100 transition-opacity duration-500 accent-in-out animate-light-travel"></span>
@@ -37,8 +49,8 @@ export const Hero = () => {
                     </button>
                 </Link>
             </div>
-        </div>
-
+       
+        </>
 
 
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,12 @@ function Navbar() {
   };
 
   return (
-    <div className="container pt-8">
+    <div className="container px-6 pt-8">
       <div className="flex justify-between items-center">
         <div className="text-xl font-medium">MY Portfolio</div>
 
         {/* Desktop Menu */}
-        <ul className="gap-10 lg:gap-16 hidden md:flex">
+        <ul className="gap-10 lg:gap-16 hidden text-lg md:flex">
           <li className="menuLink">
             <a href="#hero">Home</a>
           </li>
